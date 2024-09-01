c46d8c5d917ecc95db7d2525bf143460c636be72
JoHyoju04 <whgywn369@gmail.com>
2024-09-01 23:16:01 +0900

    feat : 숫자 맞추기 메서드 추가

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -27,4 +27,42 @@ public class BaseballGame {
         if (!isNumeric(computerNum)) throw new IllegalArgumentException();
     }
 
+    public String calculate(String input) {
+        if (!isNumeric(input)) throw new IllegalArgumentException();
+        int strikeCnt = 0;
+        int ballCnt = 0;
+        for (int i = 0; i < computerNum.length(); i++) {
+            if (computerNum.charAt(i) == input.charAt(i)) {
+                strikeCnt++;
+            } else if (computerNum.contains(String.valueOf(input.charAt(i)))) ballCnt++;
+        }
+        StringBuilder sb = new StringBuilder();
+        if (ballCnt > 0) {
+            sb.append(ballCnt);
+            sb.append(BALL);
+        }
+
+        if (strikeCnt > 0) {
+            if (sb.length() > 0) sb.append(" ");
+            sb.append(strikeCnt);
+            sb.append(STRIKE);
+        }
+        if (strikeCnt == 0 && ballCnt == 0) {
+            return NOTING;
+        }
+        return sb.toString();
+    }
+
+    boolean isNumeric(String input) {
+        if (input == null || input.length() == 0) {
+            return false;
+        }
+        try {
+            int num = Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+
+        return true;
+    }
 }
